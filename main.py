a89aeff1c0303cb6bb7911f1631114f5a3ab4e6c
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-08-31 20:46:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ sg.theme('Python')
 
 layout = [  
             [sg.InputText()],
-            [sg.Button('CE')],[sg.Button('C')], [sg.Button('X')], [sg.Button('/')]
-            [sg.Button('7')],[sg.Button('8')], [sg.Button('9')], [sg.Button('X')]
-            [sg.Button('4')],[sg.Button('5')], [sg.Button('6')], [sg.Button('-')]
-            [sg.Button('1')],[sg.Button('2')], [sg.Button('3')], [sg.Button('+')]
-            [sg.Button('()')],[sg.Button('0')], [sg.Button('.')], [sg.Button('=')]
+            [sg.Button('CE'), sg.Button('C'), sg.Button('X'), sg.Button('/')],
+            [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('X')],
+            [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-')],
+            [sg.Button('1'),sg.Button('2'), sg.Button('3'), sg.Button('+')],
+            [sg.Button('()'), sg.Button('0'), sg.Button('.'), sg.Button('=')]
         ]   
 
 window = sg.Window('Calculadora', layout)
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+    if event == sg.WIN_CLOSED or event == 'Cancael':
         break
     print('You entered ', values[0])
 
